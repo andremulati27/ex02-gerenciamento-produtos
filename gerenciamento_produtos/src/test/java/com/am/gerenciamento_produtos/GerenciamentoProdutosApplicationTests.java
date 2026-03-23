860d766fd0ecc5c39857507d078c5cbc58546f5e
Andre <andremulati@gmail.com>
2026-03-23 17:02:04 -0300

    criando model

---
+++ gerenciamento_produtos/src/test/java/com/am/gerenciamento_produtos/GerenciamentoProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.am.gerenciamento_produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
